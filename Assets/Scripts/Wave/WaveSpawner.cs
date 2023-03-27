6c6419fb28430cae2f6eda8884ab6d39f1df1301
Albi-back <94167011+Albi-back@users.noreply.github.com>
2023-03-27 20:04:51 +0100

    boss implemented +code
    - the boss moves round the arena takes damage and damages the home base and a square is being used as a placeholder

--- Assets/Scripts/Wave/WaveSpawner.cs
+++ Assets/Scripts/Wave/WaveSpawner.cs
@@ -7,7 +7,7 @@ public class WaveSpawner : MonoBehaviour
 {
     public Transform[] enemyPrefabs;
     public Transform miniBoss;
-    
+    public Transform Boss;
     public Transform spawnPoint;
 
     public float timeBetweenWaves = 5f;
@@ -64,9 +64,16 @@ public class WaveSpawner : MonoBehaviour
         
         if (PlayerStats.Rounds%5==0 && !(PlayerStats.Rounds%10==0))//checks if number is divisible by 5 but not by 10 
         {
-            Instantiate(miniBoss, spawnPoint.position, spawnPoint.rotation);// spawns miniboss
+            Instantiate(miniBoss, spawnPoint.position, spawnPoint.rotation);
             Debug.Log("Hydra");
         }
+        if ((PlayerStats.Rounds % 10 == 0))
+        {
+            Instantiate(Boss, spawnPoint.position, spawnPoint.rotation);// spawns miniboss
+        }
+
+            
+    
 
     }
 }
